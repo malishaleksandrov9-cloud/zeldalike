f4cc431f5b133328c553edd96f41ff7353c6c6a8
malishaleksandrov9-cloud <malishaleksandrov9@gmail.com>
2025-11-03 21:28:39 +0300

    start

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speedArrow;
+    public float damageForSpeed = 1;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Bow.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    public float forceArrow = 10;
+    public float cooldown = 1;
+    public float timeSnipe = 0.61f;
+    public Arrow prefab;
+    public Transform startPosition;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  Time.deltaTime 
+        if (Input.GetMouseButtonDown(0))
+        {
+            // TODO: запуск таймера
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        Arrow arrow = Instantiate(prefab, startPosition.position, startPosition.rotation);
+        arrow.rb.AddForce(startPosition.up * forceArrow);
+    }
+
+    public void Scope()
+    {
+
+    }
+}
