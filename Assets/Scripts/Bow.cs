f4aae35be077b7efc11fd7cef3379d527bbbe095
malishaleksandrov9-cloud <malishaleksandrov9@gmail.com>
2025-11-10 20:39:47 +0300

    bow done

--- Assets/Scripts/Bow.cs
+++ Assets/Scripts/Bow.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Bow : MonoBehaviour
 {
     public float forceArrow = 10;
+    public float destroyTime = 20;
     public float cooldown = 1;
     public float timeSnipe = 0.61f;
     public Arrow prefab;
@@ -30,7 +31,8 @@ public class Bow : MonoBehaviour
     public void Shoot()
     {
         Arrow arrow = Instantiate(prefab, startPosition.position, startPosition.rotation);
-        arrow.rb.AddForce(startPosition.up * forceArrow);
+        arrow.rb.AddForce(startPosition.up * forceArrow,ForceMode.Impulse);
+        Destroy(arrow, destroyTime);
     }
 
     public void Scope()
